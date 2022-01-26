64faff1af3325cca798e69cf291aaf1fe779339b
zloreno <zlorenobonaiuti@gmail.com>
2022-01-26 14:34:52 +0100

    added middleware - logger

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const generes = require('./routes/genres');
 const homepage = require('./routes/homepage');
+const logger = require('./middlware/logger');
+
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 // For every request that points towards this suffix, use that router
 app.use('/api/generes', generes);
 app.use('/', homepage);
