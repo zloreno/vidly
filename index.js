d824cef5589ee31d4dd4ed3688494d5d0f80b06e
zloreno <zlorenobonaiuti@gmail.com>
2022-01-27 14:29:47 +0100

    Route to Customers

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(express.json());
 // For every request that points towards this suffix, use that router
 app.use('/api/generes', generes);
 app.use('/', homepage);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/customers.js
+++ routes/customers.js
@@ -17,9 +17,18 @@ function validateCustomers(req) {
 	return schema.validate(req.body);
 }
 
+function validateCustomerObject(obj) {
+	const schema = Joi.object({
+		isGold: Joi.boolean().required(),
+		name: Joi.string().required(),
+		phone: Joi.string().required().min(5),
+	});
+	return schema.validate(obj);
+}
+
 // Validation of the data in input
 const customerSchema = new mongoose.Schema({
-	isGold: { type: Boolean, required: true },
+	isGold: { type: Boolean, required: true, default: false },
 	name: { type: String, required: true },
 	phone: { type: String, required: true, minlength: 5, maxlength: 20 },
 });
@@ -28,7 +37,7 @@ const Customer = mongoose.model('customers', customerSchema);
 
 //---------------------------------------------------------------- GET
 router.get('/', async (req, res) => {
-	const customer = await Customer.find();
+	const customer = await Customer.find().sort('name');
 	res.send(customer);
 });
 
@@ -59,23 +68,31 @@ router.post('/', async (req, res) => {
 
 //---------------------------------------------------------------- PUT
 router.put('/:id', async (req, res) => {
-	const { error } = validateCustomers(req);
-	if (error) return res.status(400).send(error.details[0].message);
-
-	Customer.findOneAndUpdate(
+	const oldCustomer = await Customer.findById(req.params.id);
+	let customer = await Customer.findOneAndUpdate(
 		{ _id: req.params.id },
-		{ $set: { isGold: isGold, name: name, phone: phone } }
-	).exec(function (err, book) {
-		if (err) {
-			console.log(err);
-			res.status(500).send(err);
-		} else {
-			res.status(200).send(customer);
+		{
+			$set: {
+				isGold: req.body.isGold || oldCustomer.isGold,
+				name: req.body.name || oldCustomer.name,
+				phone: req.body.phone || oldCustomer.params,
+			},
 		}
-	});
+	);
+	// Missing Joi validator
+	if (!customer)
+		return res.status(404).send(`Customer with ID ${req.params.id} not found`);
+
+	return res.send(customer);
 });
 
 //---------------------------------------------------------------- DELETE
+router.delete('/:id', async (req, res) => {
+	const deletedCustomer = await Customer.findByIdAndDelete(req.params.id);
+	if (!deletedCustomer)
+		return res.status(404).send(`Customer with ID ${req.params.id} not found`);
+	res.send(deletedCustomer);
+});
 
 //---------------------------------------------------------------- Exports
 module.exports = router;
