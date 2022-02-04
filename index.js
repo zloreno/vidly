cd0a948cc6f6e17594ed111a784a3db18c5cb133
zloreno <zlorenobonaiuti@gmail.com>
2022-02-04 19:36:24 +0100

    refactored logging logic

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // modules
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
 const config = require('config');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+// require logging first just in case anything fails
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
@@ -14,32 +13,6 @@ if (!config.get('jwtPrivateKey')) {
 	console.log(`FATAL ERROR: jwtPrivateKey is not defined`);
 	process.exit(1);
 }
-
-winston.add(
-	new winston.transports.File({
-		filename: 'logfile.log',
-	})
-);
-winston.add(
-	new winston.transports.MongoDB({
-		db: 'mongodb://localhost/vidly',
-		level: 'info',
-	})
-);
-
-// When we have an exception in the Node Environment but nowhere to handle such error
-// Works only where there are NOT promises involved
-process
-	.on('unhandledRejection', (reason, p) => {
-		console.error(reason, '\n\nUnhandled Rejection at Promise:\n', p);
-		winston.error(reason, reason);
-		process.exit(1);
-	}) //winston.handleExceptions( new winston.transports.File({filename: 'uncaughtExceptions.log}))
-	.on('uncaughtException', (err) => {
-		console.error(err, 'Uncaught Exception thrown');
-		winston.error(err.message, err);
-		process.exit(1);
-	});
 
 // functions
 // npm i joi-object-id
--- startup/logging.js
+++ startup/logging.js
@@ -1,1 +1,31 @@
-module.exports = function () {};
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
+
+module.exports = function () {
+	winston.add(
+		new winston.transports.File({
+			filename: 'logfile.log',
+		})
+	);
+	winston.add(
+		new winston.transports.MongoDB({
+			db: 'mongodb://localhost/vidly',
+			level: 'info',
+		})
+	);
+
+	// When we have an exception in the Node Environment but nowhere to handle such error
+	// Works only where there are NOT promises involved
+	process
+		.on('unhandledRejection', (reason, p) => {
+			console.error(reason, '\n\nUnhandled Rejection at Promise:\n', p);
+			winston.error(reason, reason);
+			process.exit(1);
+		}) //winston.handleExceptions( new winston.transports.File({filename: 'uncaughtExceptions.log}))
+		.on('uncaughtException', (err) => {
+			console.error(err, 'Uncaught Exception thrown');
+			winston.error(err.message, err);
+			process.exit(1);
+		});
+};
